a09e4816be04438551c9cdfd6526585660fb2954
uscin20 <105509714+uscin20@users.noreply.github.com>
2026-01-15 12:02:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,22 @@ function checkpassword() {
                 alert("Niepoprawne polecenie.");
             }
         }
+
+var roundButton = document.querySelector("#roundButton");
+        roundButton.addEventListener("click", showMenu, false);
+
+        var flyoutMenu = document.querySelector("#flyoutMenu");
+        flyoutMenu.addEventListener("click", hideMenu, false);
+
+        function showMenu(e) {
+            flyoutMenu.classList.add("show");
+
+            document.body.style.overflow = "hidden";
+        }
+
+        function hideMenu(e) {
+            flyoutMenu.classList.remove("show");
+            e.stopPropagation();
+
+            document.body.style.overflow = "auto";
+        }
