32e914bd350527ffcb2c8da4601fa5039da0fe53
uscin20 <105509714+uscin20@users.noreply.github.com>
2026-01-15 11:57:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+// Uzyskiwanie dostępu do przycisku i menu
+const menuToggle = document.getElementById('menuToggle');
+const sideMenu = document.getElementById('sideMenu');
+
+// Funkcja do otwierania i zamykania menu
+function toggleMenu() {
+  if (sideMenu.style.left === '-250px') {
+    sideMenu.style.left = '0';
+  } else {
+    sideMenu.style.left = '-250px';
+  }
+}
+
+// Obsługuje kliknięcie na przycisk, aby przełączać widoczność menu
+menuToggle.addEventListener('click', toggleMenu);
+
+// Obsługuje kliknięcie poza menu – zamyka menu, jeśli jest otwarte
+window.addEventListener('click', function(event) {
+  if (!sideMenu.contains(event.target) && event.target !== menuToggle) {
+    sideMenu.style.left = '-250px'; // Zamyka menu, jeśli kliknięto poza nim
+  }
+});
+
+function checkpassword() {
+           var password = document.getElementById("password").value;
+            
+            if (password === "kod") {
+                var userResponse = confirm("html");
+                if (userResponse) {
+                    window.location.href = "https://glitch.com/edit/#!/265wikingowie/";
+                } else {
+                    alert("Anulowano.");
+                }
+            } else if (password === "file") {
+                var userResponse = confirm("plany");
+                if (userResponse) {
+                    window.location.href = "https://drive.google.com/drive/folders/1WyLGPgrNm4YLouJUlhp4mJqUvfc-h3yr?usp=sharing";
+                } else {
+                    alert("Anulowano.");
+                }
+            } else if (password === "nic") {
+                window.location.href = "about:blank";
+            } else {
+                alert("Niepoprawne polecenie.");
+            }
+        }
